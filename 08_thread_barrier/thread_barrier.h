87f8017ba2983776b22d158bb84728f7001b70f5
maherme <manuel.hernandez.mendez@gmail.com>
2024-03-01 16:48:32 +0100

    Add some comments to thread barrier example

--- 08_thread_barrier/thread_barrier.h
+++ 08_thread_barrier/thread_barrier.h
@@ -6,12 +6,12 @@
 
 typedef struct
 {
-    unsigned int threshold_count;
-    unsigned int current_wait_count;
-    pthread_cond_t cv;
-    pthread_mutex_t mutex;
-    bool is_ready_again;
-    pthread_cond_t busy_cv;
+    unsigned int threshold_count;       /**< @brief Maximum number of blocked threads */
+    unsigned int current_wait_count;    /**< @brief Current number of blocked threads */
+    pthread_cond_t cv;                  /**< @brief CV where threads will be blocked */
+    pthread_mutex_t mutex;              /**< @brief For managing the mutual exclusion of the data struct */
+    bool is_ready_again;                /**< @brief Tracks if the barrier dispositon is on going (false) */
+    pthread_cond_t busy_cv;             /**< @brief Blocks arriving threads if disposition is on going */
 }thread_barrier_t;
 
 void
